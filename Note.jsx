54a9a1e08684d95d42b3be3a944cb3ccf0d606b1
MeghKSaji <85431594+MeghKSaji@users.noreply.github.com>
2021-06-06 13:30:20 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+ return(
+   <div className="note"> 
+     <h1> Javascript and React.js</h1>
+     <p>
+       This was an amazing bootcamo taken by shauraya sinha 
+       we covered everything from scratch including 
+       javascript .react.js,html.
+       </p>
+     </div>
+ ) ; 
+}
+export default Note; 
